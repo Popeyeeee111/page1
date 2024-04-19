7df8907e1ebcb187c69cb3505bf585412aa7c297
li Jin <duzw03@gmail.com>
2024-04-19 13:37:52 +0800

    element

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-
+  <div>
+    <el-row>
+      <el-col :span="8">
+        1
+      </el-col>
+      <el-col :span="8">
+        2
+      </el-col>
+      <el-col :span="8">
+        3
+      </el-col>
+    </el-row>
+    <el-button type="primary">Primary</el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style lang="scss">
+
+</style>
